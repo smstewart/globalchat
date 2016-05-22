1a3149bdec1c732fb954cde228fe5f9dc78cf248
Scott Stewart <smstewart91@gmail.com>
2016-05-22 10:49:51 -0700

    add auto polling

--- src/main/webapp/js/chatroom.js
+++ src/main/webapp/js/chatroom.js
@@ -1,5 +1,29 @@
 // Scroll function
-
+function scrollToBottom() {
+	$("#chatBox").scrollTop($("#chatBox")[0].scrollHeight);
+}
 // Async post function
 
+// Polling function
+function poll() {
+	window.setTimeout(updateMessages, 1000);
+}
 // Refresh function
+function updateMessages() {
+	var params = {update: getLastUpdated()};
+	$.get(
+		"/", 
+		params,
+		function(data) {
+			$("#chatBox").append(data);
+			scrollToBottom();
+		},
+		"html"
+	);
+	poll();
+}
+// Get last updated time
+function getLastUpdated() {
+	var message = $(".message:last");
+	return message.find("#lastUpdate").val();	
+}
